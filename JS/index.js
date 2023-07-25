6ce8cf1b808e1b20ddb8f8b6e31739e6d340573f
Miguel Guillén Paniagua <139347725+miguguillen@users.noreply.github.com>
2023-07-25 23:10:12 +0200

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+  window.sr = ScrollReveal();
+  sr.reveal('.hero', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.hero__container', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
